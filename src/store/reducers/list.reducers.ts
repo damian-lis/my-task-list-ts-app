195b21cdfb19d4c1582b7facf5b817f7cd967f52
Damian Lis <foxprogramming93@gmail.com>
2021-06-22 16:36:13 +0200

    Created list reducer with add list case logic

---
+++ src/store/reducers/list.reducers.ts
@@ -0,0 +1,36 @@
+import { ListsAction, ListState, Lists, ADD_LIST } from '../types';
+
+const initialState: ListState = {
+  lists: {},
+};
+
+// Helper functions
+const getListsFromLS = (): Lists => {
+  if (localStorage.getItem('task_list')) {
+    return JSON.parse(localStorage.getItem('task_list') || '{}');
+  }
+
+  return {};
+};
+
+const saveListsToLS = (lists: Lists) => {
+  localStorage.setItem('task_list', JSON.stringify(lists));
+};
+
+export default (state = initialState, action: ListsAction): ListState => {
+  const listsFromLS = getListsFromLS();
+
+  switch (action.type) {
+    case ADD_LIST:
+      const clonedListsFromLS = { ...listsFromLS };
+      clonedListsFromLS[action.payload.id] = action.payload;
+      saveListsToLS(clonedListsFromLS);
+      return {
+        ...state,
+        lists: clonedListsFromLS,
+      };
+
+    default:
+      return state;
+  }
+};
