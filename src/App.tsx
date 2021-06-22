f98bd378516cb7d6a5e4b59ed43350bbedb88960
Damian Lis <foxprogramming93@gmail.com>
2021-06-22 19:47:16 +0200

    Added Notification component to App.tsx structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { RootState } from './store/store';
 import Header from 'components/Header';
 import Sidebar from 'components/Sidebar';
+import Notification from 'components/Notification';
 
 const App: FC = () => {
+  const notificationMsg = useSelector(
+    (state: RootState) => state.notification.message
+  );
   return (
-    <div>
+    <div className='app'>
       <Header
         title='Task List App'
         subtitle='Create some lists and add some tasks to each list'
@@ -15,6 +21,7 @@ const App: FC = () => {
           <Sidebar />
         </div>
       </div>
+      <Notification msg={notificationMsg} />
     </div>
   );
 };
