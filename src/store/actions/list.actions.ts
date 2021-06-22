db67bf2b598d74656918118dfc3ca0ad4f712f40
Damian Lis <foxprogramming93@gmail.com>
2021-06-22 20:03:12 +0200

    Created getLists action

--- src/store/actions/list.actions.ts
+++ src/store/actions/list.actions.ts
@@ -1,8 +1,14 @@
-import { List, ListsAction, ADD_LIST } from 'store/types';
+import { List, ListsAction, ADD_LIST, GET_LISTS } from 'store/types';
 
 export const addList = (list: List): ListsAction => {
   return {
     type: ADD_LIST,
     payload: list,
+  };
+};
+
+export const getLists = (): ListsAction => {
+  return {
+    type: GET_LISTS,
   };
 };
