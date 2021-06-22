8058959bda0db572980a95c569a64854640bd8da
Damian Lis <foxprogramming93@gmail.com>
2021-06-22 16:34:27 +0200

    Added types related to adding a new list to redux store

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,3 +1,5 @@
+export const ADD_LIST = 'ADD_LIST';
+
 export interface Task {
   name: string;
   id: string;
@@ -8,4 +10,21 @@ export interface List {
   name: string;
   id: string;
   tasks: Task[];
+}
+
+export interface Lists {
+  [id: string]: List;
+}
+
+//Actions
+
+interface AddListAction {
+  type: typeof ADD_LIST;
+  payload: List;
+}
+
+export type ListsAction = AddListAction;
+
+export interface ListState {
+  lists: Lists;
 }
