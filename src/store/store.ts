c7a13785670701073ef2e6e3be0d50f857a4e8d5
Damian Lis <foxprogramming93@gmail.com>
2021-06-22 19:38:10 +0200

    Created notification reducer with SET_NOTIFICATION action type

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import listReducer from 'store/reducers/list.reducers';
+import notificationReducer from 'store/reducers/notification.reducers';
 
 const rootReducer = combineReducers({
   list: listReducer,
+  notification: notificationReducer,
 });
 
 const store = createStore(rootReducer, composeWithDevTools());
