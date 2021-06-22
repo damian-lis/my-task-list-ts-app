dbc3b3ce62bbbeb7b18bdba662001af6a22e20d5
Damian Lis <foxprogramming93@gmail.com>
2021-06-22 16:02:33 +0200

    Created Sidebar and CreateNewList components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import './App.css';
 import Header from 'components/Header';
+import Sidebar from 'components/Sidebar';
 
 const App: FC = () => {
   return (
@@ -9,6 +10,11 @@ const App: FC = () => {
         title='Task List App'
         subtitle='Create some lists and add some tasks to each list'
       />
+      <div className='container px-5'>
+        <div className='columns'>
+          <Sidebar />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import CreateNewList from './CreateNewList';
+
+const Sidebar: FC = () => {
+  return (
+    <div className='column is-3'>
+      <CreateNewList />
+    </div>
+  );
+};
+
+export default Sidebar;
