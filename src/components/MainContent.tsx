71922facf157945b042fbf0640e9840e4c291834
Damian Lis <foxprogramming93@gmail.com>
2021-06-23 13:16:51 +0200

    Added AddNewTask component to MainContent component

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/store';
 import SelectList from './SelectList';
+import AddNewTask from './AddNewTask';
 
 const MainContent: FC = () => {
+  const selectedList = useSelector(
+    (state: RootState) => state.list.selectedList
+  );
+
   return (
     <div className='column is-9'>
       <div className='box'>
-        <SelectList />
+        <div className='box'>
+          <SelectList />
+          {selectedList && <AddNewTask list={selectedList} />}
+        </div>
       </div>
     </div>
   );
