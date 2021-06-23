35ad2650bbc803215c9d6ea1fb9945273b8ffa89
Damian Lis <foxprogramming93@gmail.com>
2021-06-23 10:42:33 +0200

    Added some types related to list delete

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,5 +1,8 @@
 export const ADD_LIST = 'ADD_LIST';
 export const GET_LISTS = 'GET_LISTS';
+export const GET_LIST_BY_ID = 'GET_LIST_BY_ID';
+export const DELETE_LIST = 'DELETE_LIST';
+export const SET_LIST_ID_TO_DELETE = 'SET_LISTID_TO_DELETE';
 
 export const SET_NOTIFICATION = 'SET_NOTIFICATION';
 
@@ -30,6 +33,21 @@ interface GetListsAction {
   type: typeof GET_LISTS;
 }
 
+interface GetListByIdAction {
+  type: typeof GET_LIST_BY_ID;
+  payload: string;
+}
+
+interface DeleteListAction {
+  type: typeof DELETE_LIST;
+  payload: string;
+}
+
+interface SetListIdToDeleteAction {
+  type: typeof SET_LIST_ID_TO_DELETE;
+  payload: string;
+}
+
 interface SetNotificationAction {
   type: typeof SET_NOTIFICATION;
   payload: {
@@ -38,12 +56,20 @@ interface SetNotificationAction {
   };
 }
 
-export type ListsAction = AddListAction | GetListsAction;
+export type ListsAction =
+  | AddListAction
+  | GetListsAction
+  | GetListByIdAction
+  | DeleteListAction
+  | SetListIdToDeleteAction;
 
 export type NotificationAction = SetNotificationAction;
 
 export interface ListState {
   lists: Lists;
+  listIdToDelete: string;
+  listById: List | null;
+  selectedList: List | null;
 }
 
 export interface NotificationState {
