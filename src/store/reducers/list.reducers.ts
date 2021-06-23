6857adec46c9243a3ce9b430427d97f370b2f03d
Damian Lis <foxprogramming93@gmail.com>
2021-06-23 10:44:09 +0200

    Added some cases in list reducer related to list delete

--- src/store/reducers/list.reducers.ts
+++ src/store/reducers/list.reducers.ts
@@ -1,7 +1,19 @@
-import { ListsAction, ListState, Lists, ADD_LIST, GET_LISTS } from '../types';
+import {
+  ListsAction,
+  ListState,
+  Lists,
+  ADD_LIST,
+  GET_LISTS,
+  GET_LIST_BY_ID,
+  DELETE_LIST,
+  SET_LIST_ID_TO_DELETE,
+} from 'store/types';
 
 const initialState: ListState = {
   lists: {},
+  listIdToDelete: '',
+  listById: null,
+  selectedList: null,
 };
 
 // Helper functions
@@ -34,6 +46,35 @@ export default (state = initialState, action: ListsAction): ListState => {
       return {
         ...state,
         lists: listsFromLS,
+      };
+
+    case GET_LIST_BY_ID:
+      const list = listsFromLS[action.payload];
+      return {
+        ...state,
+        listById: list,
+      };
+
+    case SET_LIST_ID_TO_DELETE:
+      return {
+        ...state,
+        listIdToDelete: action.payload,
+      };
+
+    case DELETE_LIST:
+      const clonedListsFromLS2 = { ...listsFromLS };
+      const listId = clonedListsFromLS2[action.payload].id;
+      delete clonedListsFromLS2[action.payload];
+      saveListsToLS(clonedListsFromLS2);
+      return {
+        ...state,
+        lists: clonedListsFromLS2,
+        listIdToDelete: '',
+        listById: null,
+        selectedList:
+          state.selectedList && listId === state.selectedList.id
+            ? null
+            : state.selectedList,
       };
 
     default:
