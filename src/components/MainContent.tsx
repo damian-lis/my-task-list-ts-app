80ddd797e7a5fd60ae05b6287b37e0741ef1e984
Damian Lis <foxprogramming93@gmail.com>
2021-06-23 12:40:44 +0200

    Created MainContent component and added to it SelectList component

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import SelectList from './SelectList';
+
+const MainContent: FC = () => {
+  return (
+    <div className='column is-9'>
+      <div className='box'>
+        <SelectList />
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
