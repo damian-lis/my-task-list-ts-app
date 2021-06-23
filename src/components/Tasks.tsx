0513763da40dcd991c8bdfb5ff3fad98ed656c94
Damian Lis <foxprogramming93@gmail.com>
2021-06-23 14:06:54 +0200

    Set the option to dispatch the setTaskToEdit action in Tasks component

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Task } from 'store/types';
+import { setTaskToEdit } from 'store/actions';
+import { RootState } from 'store/store';
 
 interface TasksProps {
   tasks: Task[];
 }
 
 const Tasks: FC<TasksProps> = ({ tasks }) => {
+  const dispatch = useDispatch();
+  const list = useSelector((state: RootState) => state.list.selectedList!);
+
+  const setTaskToEditHandler = (task: Task) => {
+    dispatch(setTaskToEdit(task, list));
+  };
+
   return (
     <section className='section'>
       <h2 className='is-size-4 has-text-centered'>
@@ -27,7 +37,10 @@ const Tasks: FC<TasksProps> = ({ tasks }) => {
               <tr key={task.id} className={task.completed ? 'completed' : ''}>
                 <td>{task.name}</td>
                 <td className='has-text-centered'>
-                  <button className='button is-primary is-small'>
+                  <button
+                    className='button is-primary is-small'
+                    onClick={() => setTaskToEditHandler(task)}
+                  >
                     <span className='icon'>
                       <i className='fas fa-edit'></i>
                     </span>
